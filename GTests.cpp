99882664d3566c1897857c9ef6000c8a32058070
jo22i <99878853+jo22i@users.noreply.github.com>
2022-06-03 04:26:18 +0300

    Написаны основные mock-объекты

--- GTests.cpp
+++ GTests.cpp
@@ -3,23 +3,27 @@
 #include "./banking/Account.h"
 #include "./banking/Transaction.h"
 
-//Добавить модули для проверки Account
-class MockAccount: public Acount
+class MockAccount: public Account
 {
-	MOCK_METHOD(Account, ());
+	MOCK_METHOD(Account, (int id, int balance));
+	MOCK_METHOD(~Account, (), (override));
 	MOCK_METHOD(GetBalance, (), (override));
 	MOCK_METHOD(ChangeBalance, (int diff), (override));
 	MOCK_METHOD(Lock, (), (override));
 	MOCK_METHOD(Unlock, (), (override));
-	MOCK_METHOD(~Account, (), (override));
+	MOCK_METHOD(id, ());
 }
 
 class MockTransaction: public Transaction
 {
+	MOCK_METHOD(Transaction, ());
 	MOCK_METHOD(~Transaction, (), (override));
-	MOCL_METHOD(Transaction, ());
+  MOCK_METHOD(Make, (Account& from, Account& to, int sum));
+  MOCK_METHOD(fee, ());
+  MOCK_METHOD(set_fee, (int fee));
+}
 
-  	bool Make(Account& from, Account& to, int sum);
-  	int fee() const { return fee_; }
-  	void set_fee(int fee) { fee_ = fee; }
+TEST(Account, MockTest)
+{
+  
 }
