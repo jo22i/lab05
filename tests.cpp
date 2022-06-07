3755cad2e5fccd77f0f9a608f2a94a483f8377c3
хто я <99878853+jo22i@users.noreply.github.com>
2022-06-07 17:58:58 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -11,10 +11,10 @@ class MockAccount: public Account
 {
 public:
     MockAccount(int id, int balance) : Account::Account(id, balance) {}
-    MOCK_METHOD(void, Account, (int id, int balance));
+    //MOCK_METHOD(void, Account, (int id, int balance));
     //MOCK_METHOD(void, ~Account, (), (override));
     MOCK_METHOD(int, GetBalance, (), (const, override));
-    MOCK_METHOD(void, ChangeBalance, (int diff), (override)) { this->Account::ChangeBalance(int diff) };
+    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
     MOCK_METHOD(void, Lock, (), (override));
     MOCK_METHOD(void, Unlock, (), (override));
     MOCK_METHOD(int, id, (), (const));
@@ -26,9 +26,9 @@ class MockTransaction: public Transaction
 public:
     MOCK_METHOD(void, Transaction, ());
     //MOCK_METHOD(void, ~Transaction, (), (override));
-    MOCK_METHOD(bool, Make, (Account& from, Account& to, int sum)) { this->Transaction::Make(Account& from, Account& to, int sum) };
-    MOCK_METHOD(int, fee, (), (const)) { this->Transaction::fee() };
-    MOCK_METHOD(void, set_fee, (int fee)) { this->Transaction::set_fee(int fee) };
+    MOCK_METHOD(bool, Make, ((Account& from), (Account& to), (int sum)));
+    MOCK_METHOD(int, fee, (), (const));
+    MOCK_METHOD(void, set_fee, (int fee));
 };
 
 // Testing basic features of the "MockAccount" class
